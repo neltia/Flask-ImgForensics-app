ced0b8f97a20a3709670fb111adc53987b6f9990
neltia <dsz08082@naver.com>
2022-01-15 02:08:29 +0900

    upload & exif 처리 완료

--- info.py
+++ info.py
@@ -2,6 +2,8 @@
 from public_lib import *
 from flask import request
 # addtion lib
+from werkzeug.utils import secure_filename
+import exifread
 import sys
 import hashlib
 import os
@@ -16,13 +18,22 @@ api = Namespace(
 
 
 # info-1. 이미지 파일 업로드
-@api.route(f'/{api_ver}/image')
+@api.route(f'/{api_ver}/upload')
 class img_info(Resource):
     def post(self):
+        # 파일 저장
         raw_file = request.files['file']
-        filename = raw_file.filename
+        filename = secure_filename(raw_file.filename)
         raw_file.save(f"./uplodas/{filename}")
+
+        # 파일 이름 SHA-256 값으로 변경
         raw_data = raw_file.read()
+        filetype = os.path.splitext(filename)[-1]
+        hash_sha256 = hashlib.sha256(raw_data).hexdigest()
+        raw_file.close()
+        os.rename(f"{path_dir}/{filename}", f"{path_dir}/{hash_sha256}{filetype}")
+
+        # Result
         msg = "Success"
         result = {
             "result_code": 0,
@@ -30,18 +41,49 @@ class img_info(Resource):
             "filename": filename,
             "md5": hashlib.md5(raw_data).hexdigest(),
             "sha1": hashlib.sha1(raw_data).hexdigest(),
-            "sha256": hashlib.sha256(raw_data).hexdigest()
+            "sha256": hash_sha256,
+            "filesize": os.path.getsize(f"{path_dir}/{filename}"),
+            "filetype": filetype[1:]
         }
         return result
 
 
 # info-2. 이미지 파일 EXIF 데이터 확인
-@api.route(f'/{api_ver}/exif')
+@api.route(f'/{api_ver}/image')
 class img_exif(Resource):
     def get(self):
+        result = {}
+
+        # 파라미터 파싱
+        params = request.args.to_dict()
+        if "hash" not in params:
+            result["result_code"] = -1
+            result["msg"] = "필수 요청 파라미터 값이 없습니다."
+            return result
+        filehash = params["hash"]
+
+        # 파일 존재 여부 검사
+        file_list = os.listdir(path_dir)
+        name_list = [os.path.splitext(fullname)[0] for fullname in file_list]
+        if filehash not in name_list:
+            result["result_code"] = -1
+            result["msg"] = "해당하는 파일이 없습니다."
+            return result
+
+        # EXIF Parsing
+        filename = file_list[name_list.index(filehash)]
+        raw_data = open(f"{path_dir}/{filename}", 'rb')
+        tags = exifread.process_file(raw_data)
+        raw_data.close()
+
+        # Result
         msg = "test"
         result = {
             "result_code": 0,
-            "result_message": msg
+            "result_message": msg,
+            "sha256": filehash,
+            "filesize": os.path.getsize(f"{path_dir}/{filename}"),
+            "filetype": os.path.splitext(filename)[-1][1:],
+            "exif_tags": str(tags)
         }
         return result
--- public_lib.py
+++ public_lib.py
@@ -2,6 +2,8 @@
 from flask import jsonify
 from flask_restx import Resource
 from flask_restx import Namespace
+import os
 
 # develop api version
 api_ver = "0.0.1"
+path_dir = os.path.abspath(f"./uplodas")
